e4e2652a7bd37dcc02dbdb7bfa9d614ceb9d1dbf
nazmeln <nazik100378@gmail.com>
2020-07-29 12:00:27 +0300

    added animation

--- src/main.js
+++ src/main.js
@@ -52,3 +52,36 @@ const bookButton = document.querySelector('.header__button');
 bookButton.addEventListener('click', () => (
   window.location = '#contact'
 ));
+
+// animation
+function animation() {
+  let elements;
+  let windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (let i = 0; i < elements.length; i++) {
+      setTimeout(() => {
+        const element = elements[i];
+        const positionFromTop = elements[i].getBoundingClientRect().top;
+
+        if (positionFromTop - windowHeight <= 0) {
+          element.classList.add('fade-in-element');
+          element.classList.remove('hidden');
+        }
+      }, i * 1000);
+    }
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+};
+
+animation();
